d4a6144f7abbba8e01c577d4a45dc6baff0e90cc
Shahrukh Nadeem <81955684+sharkout@users.noreply.github.com>
2023-03-24 20:46:05 +0000

    exporting app

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import Cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import Cors from "cors";
 
-import Cards from './dbCards.js';
+import Cards from "./dbCards.js";
 
 // App Config
 const app = express();
-const port = process.env.PORT || 8001
-const connection_url = 'mongodb+srv://admin:gpa3hiFpbZg3u07c@cluster0.puhfvru.mongodb.net/tinderdb?retryWrites=true&w=majority';
+const port = process.env.PORT || 8001;
+const connection_url =
+  "mongodb+srv://admin:gpa3hiFpbZg3u07c@cluster0.puhfvru.mongodb.net/tinderdb?retryWrites=true&w=majority";
 
 // Middlewares
 app.use(express.json());
@@ -17,29 +18,32 @@ app.use(Cors());
 mongoose.connect(connection_url);
 
 // API Endpoints
-app.get('/', (req, res) => res.status(200).send('hello world'));
+app.get("/", (req, res) => res.status(200).send("hello world"));
 
-app.post('/tinder/cards', async (req,res) => {
-    const dbCard = req.body;
+app.post("/tinder/cards", async (req, res) => {
+  const dbCard = req.body;
 
-    try {
-        const data = await Cards.create(dbCard);
-        res.status(201).send(data);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+  try {
+    const data = await Cards.create(dbCard);
+    res.status(201).send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-app.get('/tinder/cards', async (req, res) => {
-    try {
-        const data = await Cards.find({ });
-        res.status(200).send(data);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+app.get("/tinder/cards", async (req, res) => {
+  try {
+    const data = await Cards.find({});
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // Listener
 app.listen(port, console.log(`Listening on localhost: ${port}`));
+
+// Export the API
+export default app;
 
 // gpa3hiFpbZg3u07c
